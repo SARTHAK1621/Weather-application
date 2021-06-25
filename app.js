a9e86e3d6b54a7dd3f4ebbef79ff5567f19e6bf0
sarthak1621 <sarthaktripathy1621@gmail.com>
2021-06-25 11:29:15 +0530

    API is being called

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
+const apiKey = "a4eb999802c63cfbfdee3872f3529cb1";
+const https = require('https');
+// const inputCity = "";
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(express.static("public"));
-app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/front.html");   
 });
-app.post("/input",function(req,res)
+
+app.post("/input", function(req,res)
 {
     console.log(req.body);
-});
-app.listen(3001,function()
-{
+    const inputCity = req.body.place;
+    const resUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + apiKey;
+    https.get(resUrl, function(ress) {
+
+        // A chunk of data has been received.
+        // console.log(ress);
+        ress.on("data", function(resh){
+            //console.log(JSON.parse(resh).main.temp);
+            console.log(JSON.parse(resh));
+        });
+        // The whole response has been received. Print out the result.
+    });
+    const resUrl2="https://api.openweathermap.org/data/2.5/forecast?q="+ inputCity + "&appid=" + apiKey;
+    https.get(resUrl2,function(ress2){
+        ress2.on("data",function(resh2)
+        {
+            console.log(JSON.parse(resh2).main);
+        })
+    });
 
 });
+
+
+
+app.listen(3001,function(){});
+
