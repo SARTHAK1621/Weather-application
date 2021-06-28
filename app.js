1bdc365f3d265a16da0dff5065c68cf5291b26da
sarthak1621 <sarthaktripathy1621@gmail.com>
2021-06-28 17:43:02 +0530

    version-1

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const apiKey = "a4eb999802c63cfbfdee3872f3529cb1";
 const https = require('https');
 // const inputCity = "";
 const express = require("express");
+var expressLayouts = require('express-ejs-layouts');
 const bodyParser = require("body-parser");
 const app = express();
 app.use(express.static("public"));
-app.set("view engine","ejs")
+app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/front.html");   
@@ -14,6 +15,7 @@ app.get("/", function(req,res){
 app.post("/input", function(req,res)
 {
     console.log(req.body);
+    const name=req.body.name;
     const inputCity = req.body.place;
     const resUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + apiKey;
     https.get(resUrl, function(ress) {
@@ -34,7 +36,7 @@ app.post("/input", function(req,res)
             const humidity=JSON.parse(resh).main.humidity;
             const visibility=JSON.parse(resh).visibility;
             let suggestion="";
-            if(temp>305 && temp<273)
+            if(temp>304 && temp<273)
             {
                 suggestion="Not advised to travell";
             }
@@ -47,6 +49,7 @@ app.post("/input", function(req,res)
                 suggestion="Good condition to  travell";
             }
             console.log(suggestion);
+            res.render("result",{Name:name,Cond:cond,Desc:desc,Temp:temp,Feels:feels,Min:min_temp,Max:max_temp,Humidity:humidity,Visibility:visibility,Sugst:suggestion});
         });
         // The whole response has been received. Print out the result.
     });
