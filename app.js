8f19eb77ec37387ec821dba670ed24c031fca3a6
sarthak1621 <sarthaktripathy1621@gmail.com>
2021-07-01 15:40:41 +0530

    wrong input problem resolved

--- app.js
+++ app.js
@@ -15,41 +15,49 @@ app.get("/", function(req,res){
 app.post("/input", function(req,res)
 {
     console.log(req.body);
-    const name=req.body.name;
-    const inputCity = req.body.place;
-    const resUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + apiKey;
-    https.get(resUrl, function(ress) {
-
-        // A chunk of data has been received.
-        // console.log(ress);
-        ress.on("data", function(resh){
-            //console.log(JSON.parse(resh).main.temp);
-            console.log(JSON.parse(resh));
-            console.log(JSON.parse(resh).coord);
-            //console.log(JSON.parse(resh).weather[0].main);
-            const cond=JSON.parse(resh).weather[0].main;
-            const desc=JSON.parse(resh).weather[0].description;
-            const temp=JSON.parse(resh).main.temp;
-            const feels=JSON.parse(resh).main.feels_like;
-            const min_temp=JSON.parse(resh).main.temp_min;
-            const max_temp=JSON.parse(resh).main.temp_max;
-            const humidity=JSON.parse(resh).main.humidity;
-            const visibility=JSON.parse(resh).visibility;
-            let suggestion="";
-            console.log(temp, typeof(temp))
-            if((temp>290 || temp<273) || cond=="Rain")
-            {
-                suggestion="Not advised to travell";
-            }
-            else
-            {
-                suggestion="Good condition to  travell";
-            }
-            console.log(suggestion);
-            res.render("result",{Name:name,Cond:cond,Desc:desc,Temp:temp,Feels:feels,Min:min_temp,Max:max_temp,Humidity:humidity,Visibility:visibility,Sugst:suggestion});
+    if(req.body.place=='' || req.body.name=='')
+    {
+        res.render("front");
+    }
+    else
+    {
+        const name=req.body.name;
+        const inputCity = req.body.place;
+        const resUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + apiKey;
+        https.get(resUrl, function(ress) {
+    
+            // A chunk of data has been received.
+            // console.log(ress);
+            ress.on("data", function(resh){
+                //console.log(JSON.parse(resh).main.temp);
+                console.log(JSON.parse(resh));
+                console.log(JSON.parse(resh).coord);
+                //console.log(JSON.parse(resh).weather[0].main);
+                const cond=JSON.parse(resh).weather[0].main;
+                const desc=JSON.parse(resh).weather[0].description;
+                const temp=JSON.parse(resh).main.temp;
+                const feels=JSON.parse(resh).main.feels_like;
+                const min_temp=JSON.parse(resh).main.temp_min;
+                const max_temp=JSON.parse(resh).main.temp_max;
+                const humidity=JSON.parse(resh).main.humidity;
+                const visibility=JSON.parse(resh).visibility;
+                let suggestion="";
+                console.log(temp, typeof(temp))
+                if((temp>290 || temp<273) || cond=="Rain")
+                {
+                    suggestion="Not advised to travell";
+                }
+                else
+                {
+                    suggestion="Good condition to  travell";
+                }
+                console.log(suggestion);
+                res.render("result",{Name:name,Cond:cond,Desc:desc,Temp:temp,Feels:feels,Min:min_temp,Max:max_temp,Humidity:humidity,Visibility:visibility,Sugst:suggestion});
+            });
+            // The whole response has been received. Print out the result.
         });
-        // The whole response has been received. Print out the result.
-    });
+    }
+    
     // const resUrl2="https://api.openweathermap.org/data/2.5/forecast?q="+ inputCity + "&appid=" + apiKey;
     // https.get(resUrl2,function(ress2){
     //     ress2.on("data",function(resh2)
